ead29406795beb5ccb7b6927b7ce052bb45a230f
George Wahba <54804560+georgewahba@users.noreply.github.com>
2025-10-03 10:10:43 +0200

    opdracht 2a

--- BAI2/SetFuncs.cs
+++ BAI2/SetFuncs.cs
@@ -8,21 +8,62 @@ namespace BAI
         {
             // *** IMPLEMENTATION HERE *** //
             // Hier mag je loopen over pixeldata
-            return new HashSet<uint>();
+            // hier maak ik een hashset aan die de kleuren gaat opslaan
+
+            var alleKleuren = new HashSet<uint>();
+            if (pixeldata == null) return alleKleuren;
+
+            foreach (var pixel in pixeldata)
+            {
+                alleKleuren.Add(pixel);
+            }
+            return alleKleuren;
         }
 
         public static HashSet<uint> BlauwTinten(uint[] pixeldata)
         {
             // *** IMPLEMENTATION HERE *** //
             // Hier mag je loopen over pixeldata
-            return new HashSet<uint>();
+            // Een blauwe tint is een kleur waarbij de blauwe component (bits 7-0)
+            // groter is dan zowel de rode (bits 23-16) als de groene component (bits 15-8).
+            var blauweTinten = new HashSet<uint>();
+
+            if (pixeldata == null) return blauweTinten;
+
+            foreach (var pixel in pixeldata) 
+            {
+                byte rood = PixelFuncs.RoodWaarde(pixel);
+                byte groen = PixelFuncs.GroenWaarde(pixel);
+                byte blauw = PixelFuncs.BlauwWaarde(pixel);
+                if (blauw > rood && blauw > groen)
+                {
+                    blauweTinten.Add(pixel);
+                }
+            }
+            return blauweTinten;    
         }
 
         public static HashSet<uint> DonkereKleuren(uint[] pixeldata)
         {
             // *** IMPLEMENTATION HERE *** //
             // Hier mag je loopen over pixeldata
-            return new HashSet<uint>();
+            // Een donkere kleur is een kleur waarbij de som van de
+            // rode, groene en blauwe component (bits 23-16, 15-8, 7-0)
+            // kleiner is dan 192.
+            var donkereKleuren = new HashSet<uint>();
+
+            if (pixeldata == null) return donkereKleuren;
+
+            foreach (var pixel in pixeldata) {
+                byte rood = PixelFuncs.RoodWaarde(pixel);
+                byte groen = PixelFuncs.GroenWaarde(pixel);
+                byte blauw = PixelFuncs.BlauwWaarde(pixel);
+                if ((rood + groen + blauw) < 192)
+                {
+                    donkereKleuren.Add(pixel);
+                }
+            }
+            return donkereKleuren;
         }
 
         public static HashSet<uint> NietBlauwTinten(uint[] pixeldata)
